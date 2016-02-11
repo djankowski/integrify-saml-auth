0219de54dd2e288679ba8a64d72b0a8ad0cfb51d
Rich Trusky <rtrusky@integrify.com>
2016-02-11 16:06:04 -0500

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -13,6 +13,7 @@ var config = {
             "integrify_base_url" : "https://integrify.yourdomain.com", //the url of your Integrify site
             "consumer_key": "myapikey", //your API key  (consumer_key) see https://developer.integrify.com/external-auth/activation
             "consumer_secret": "myapisecret", //your API secret (consumer_secret)
+            "tokenExpiresInMinutes" : 60, //number of minutes the loging seeiosn is valid 
             "fieldMap" : {  // a mapping of Integrify profile fields to fields returned in the SAML.
                 "NameFirst": "givenName",
                 "NameLast" : "sn",
